97da28dfd711635482f850ad9e7e617dc855035a
Your Name <you@example.com>
2020-07-20 17:13:11 +0900

    list for step 1

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template #플라스크 라이브러리로 서버를 띄우는 방법
-
+from data import Articles
 app = Flask(__name__) #대문자 플라스크는 클라스 객체생성
 app.debug = True
 
@@ -19,7 +19,9 @@ def about():
 def articles():
     print("Success")
     #return "Test"
-    return render_template('articles.html', hello = "Daechan")
+    articles = Articles()
+    print(len(articles))
+    return render_template('articles.html', articles = articles)
 
 if __name__=="__main__":
     # app.run(host = '0.0.0.0', port='50')
